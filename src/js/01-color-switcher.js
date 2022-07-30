c762eff6f18a63da156f378da077e20cc82f2931
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-07-30 15:47:15 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeBodyBgColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+function onStartBtnClick() {
+    refs.startBtn.setAttribute('disabled', 'true');
+    refs.stopBtn.removeAttribute('disabled');
+
+    timerId = setInterval(changeBodyBgColor, 1000);
+};
+
+function onStopBtnClick() {
+    refs.stopBtn.setAttribute('disabled', 'true');
+    refs.startBtn.removeAttribute('disabled');
+
+    clearInterval(timerId);
+}
